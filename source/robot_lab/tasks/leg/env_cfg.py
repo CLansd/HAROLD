7437fee74c20d43b106412ca50bfe00d2d96cb48
CLansd <11579309+CLansd@users.noreply.github.com>
2026-02-05 20:06:33 -0500

    Latest, with leg responding to inputs properly in simulation.

---
+++ source/robot_lab/tasks/leg/env_cfg.py
@@ -0,0 +1,125 @@
+### --- IMPORTS --- ###
+import math
+import isaaclab.sim as sim_utils
+from isaaclab.envs import ManagerBasedRLEnvCfg
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg
+from isaaclab.utils import configclass
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sensors import ContactSensorCfg
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.managers import ObservationGroupCfg as ObsGroup
+from isaaclab.managers import ObservationTermCfg as ObsTerm
+from isaaclab.managers import EventTermCfg as EventTerm
+from isaaclab.managers import RewardTermCfg as RewTerm
+from isaaclab.managers import TerminationTermCfg as DoneTerm
+from isaaclab.utils.noise import AdditiveGaussianNoiseCfg as GaussianNoise
+from .leg import LEG_CFG
+from .. import mdp
+
+### --- SCENE DEFINITION --- ###
+@configclass
+class LegSceneCfg(InteractiveSceneCfg):
+
+    # Robot.
+    robot: ArticulationCfg = LEG_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+    # Lights.
+    light = AssetBaseCfg(
+        prim_path="/World/SkyLight",
+        spawn=sim_utils.DomeLightCfg(
+            color=(0.9, 0.9, 0.9),
+            intensity=750.0,
+        ),
+    )
+
+### --- MDP ACTIONS --- ###
+@configclass
+class ActionsCfg:
+    joint_effort = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=["Hip", "Thigh_01", "Knee"],
+        scale=0.25,
+    )
+
+### --- MDP OBSERVATIONS --- ###
+@configclass
+class ObservationsCfg:
+
+    # Define the observation terms available to the agent.
+    @configclass
+    class PolicyCfg(ObsGroup):
+        joint_pos                   = ObsTerm(func=mdp.joint_pos_rel, history_length=10)
+        joint_vel                   = ObsTerm(func=mdp.joint_vel, history_length=10)
+
+        # Post initialization.
+        def __post_init__(self) -> None:
+            self.enable_corruption = False
+            self.concatenate_terms = True
+
+    @configclass
+    class CriticCfg(ObsGroup):
+        joint_pos                   = ObsTerm(func=mdp.joint_pos_rel, history_length=10)
+        joint_vel                   = ObsTerm(func=mdp.joint_vel, history_length=10)
+
+        robot_joint_torque          = ObsTerm(func=mdp.robot_joint_torque, history_length=10)
+        robot_joint_acc             = ObsTerm(func=mdp.robot_joint_acc, history_length=10)
+
+        # Post initialization.
+        def __post_init__(self) -> None:
+            self.enable_corruption = False
+            self.concatenate_terms = True
+
+    policy: PolicyCfg = PolicyCfg()
+    critic: CriticCfg = CriticCfg()
+
+### --- MDP EVENTS --- ###
+@configclass
+class EventCfg:
+    # ON STARTUP
+
+    # ON RESET:
+    
+    # THIS IS ABSOLUTELY CRUCIAL TO GETTING THE ROBOT TO NOT YEET ITSELF INTO THE AIR WHEN reset_root_state_uniform is enabled!!!
+    reset_robot_joints = EventTerm(
+        func=mdp.reset_joints_by_scale,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", joint_names=["Hip", "Thigh_01", "Knee"]),
+            "position_range": (1.0, 1.0), # I think we will need to adjust this.
+            "velocity_range": (0.0, 0.0),
+        },
+        is_global_time=False,
+        min_step_count_between_reset=0,
+    )
+    
+
+### --- MDP REWARDS --- ###
+@configclass
+class RewardsCfg:
+    pen_joint_torque = RewTerm(func=mdp.joint_torques_l2, weight=-0)
+
+
+### --- MDP TERMINATIONS --- ###
+@configclass
+class TerminationsCfg:
+    time_out        = DoneTerm(func=mdp.time_out, time_out=True)
+    
+
+### --- ENVIRONMENT CONFIGURATION --- ###
+@configclass
+class LegEnvCfg(ManagerBasedRLEnvCfg):
+    scene: LegSceneCfg = LegSceneCfg(num_envs=4096, env_spacing=2.5) # Default num_envs and env_spacing.
+    observations: ObservationsCfg = ObservationsCfg()
+    actions: ActionsCfg = ActionsCfg()
+    events: EventCfg = EventCfg()
+    rewards: RewardsCfg = RewardsCfg()
+    terminations: TerminationsCfg = TerminationsCfg()
+
+    # Post initialization
+    def __post_init__(self) -> None:
+        self.decimation = 4
+        self.episode_length_s = 20.0
+        self.viewer.eye = (8.0, 8.0, 4.8)
+        self.sim.dt = 0.005
+        self.sim.render_interval = 4
+        self.sim.physx.gpu_max_rigid_patch_count = 10 * 2**15
---
+++ source/robot_lab/tasks/leg/leg.py
@@ -0,0 +1,70 @@
+import isaaclab.sim as sim_utils
+import os
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from robot_lab import ROBOT_LAB_EXT_DIR
+
+### --- ARTICULATION DEFINITION --- ###
+LEG_CFG = ArticulationCfg(
+    spawn = sim_utils.UsdFileCfg(
+        usd_path=f"{ROBOT_LAB_EXT_DIR}/source/robot_lab/assets/USD_LEG_R2/USD_LEG_R2.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=4
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.0),
+        joint_pos={                 # Initial positions of the joints (radians).
+            "Hip": 0.0,
+            "Thigh_01": 0.0,
+            "Knee": 0.0,
+            },
+        joint_vel={".*": 0.0},      # Initial velocities of the joints (radians/second).
+    ),
+    soft_joint_pos_limit_factor=1.0,
+    actuators={
+        "HipActuator": ImplicitActuatorCfg(
+            joint_names_expr=["Hip"],
+            effort_limit_sim = 60.0,
+            velocity_limit_sim=19.9,
+            stiffness={
+                ".*": 60.0
+            },
+            damping={
+                ".*": 4.0
+            },
+        ),
+        "ThighActuator": ImplicitActuatorCfg(
+            joint_names_expr=["Thigh_01"],
+            effort_limit_sim=60.0,
+            velocity_limit_sim=19.9,
+            stiffness={
+                ".*": 60.0
+            },
+            damping={
+                ".*": 4.0
+            },
+        ),
+        "KneeActuator": ImplicitActuatorCfg(
+            joint_names_expr=["Knee"],
+            effort_limit_sim=60.0,
+            velocity_limit_sim=19.9,
+            stiffness={
+                ".*": 60.0
+            },
+            damping={
+                ".*": 4.0
+            },
+        ),
+    },
+)
